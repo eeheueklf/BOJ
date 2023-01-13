ae10526727110087dd8fc352818c6ba8bd43494b
eeheueklf <eeheueklf@gmail.com>
2023-01-13 15:58:19 +0900

    2023-01

---
+++ 2301/11650.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N,tx,ty;
+    cin >> N;
+
+    vector<pair<int, int>> p;
+    for(int i = 0; i < N; i++)
+    {
+        cin >> tx >> ty;
+        p.push_back(make_pair(tx, ty));
+    }
+    
+    sort(p.begin(), p.end());
+    
+    for(int i = 0; i < N; i++)
+        cout << p[i].first << " " << p[i].second << "\n";
+}
